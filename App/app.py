9b81183a5dd8849f631b1232fc7dcc14250ba051
Ramkumar <71488613+Ramkumar-datascience@users.noreply.github.com>
2021-08-05 06:16:20 -0700

    Mail Automation V2.0

--- App/app.py
+++ App/app.py
@@ -13,36 +13,54 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/mail', methods=['GET', 'POST'])
 def mailing():
-    fromaddr = request.form.get('sender')
-    toaddr = request.form.get('receiver')
-    msg = MIMEMultipart() 
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Tesing Mail"
-    body = "Congratulations! You Got Shortlisted"
-    msg.attach(MIMEText(body, 'plain'))
-    filename = "file.pdf"
-    attachment = open("D:/Ramkumar/Certificate_name_edit/DM/file.pdf", "rb")
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload((attachment).read())
-    encoders.encode_base64(p)
-       
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    msg.attach(p)
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    pwd = request.form.get('password')
-    s.login(fromaddr, pwd)
-    text = msg.as_string()
-    s.sendmail(fromaddr, toaddr, text)
-    s.quit()
-    confirm = 'Mail Has been sent'
+    if request.method == 'POST':
+        mails_list = []
+        file = request.form['upload-file']
+        #print("FIle",file)
+        df = pd.read_csv(file)
+        #print(df)
+        dic = dict(zip(df.mails,df.names))
+        count= 1
+        for i in dic:
+            print(count," : ",i)
+            fromaddr = request.form.get('sender')
+            toaddr = i
+            msg = MIMEMultipart() 
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = request.form.get('subject')
+            f = request.form['upload-body']
+            f = open(f,'r')
+            read = f.read()
+            body = read.format(dic[i])
+            log = open("logs.csv",'a')
+            log.write(i)
+            log.write("\n")
+            #print("Body :",body)
+            msg.attach(MIMEText(body, 'plain'))
+            filename = "ram.jpeg"
+            attachment = open("ram.jpeg", "rb")
+            p = MIMEBase('application', 'octet-stream')
+            p.set_payload((attachment).read())
+            encoders.encode_base64(p)
+               
+            p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+            msg.attach(p)
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.starttls()
+            pwd = request.form.get('password')
+            s.login(fromaddr, pwd)
+            text = msg.as_string()
+            s.sendmail(fromaddr, toaddr, text)
+            s.quit()
+            count += 1
+        confirm = 'Mail Has been sent'
     
-    return render_template("index.html",confirm = confirm)
+        return render_template("home.html",confirm = confirm)
     
 
 @app.route('/import',methods = ['GET', 'POST'])
@@ -81,7 +99,7 @@ def importCsv():
             s.sendmail(fromaddr, toaddr, text)
             s.quit()
         confirm = 'Mail Has been sent'
-        return render_template("index.html", confirm = confirm)
+        return render_template("home.html", confirm = confirm)
 
 # Mail for only bulk mails with common content       
 @app.route('/textmail', methods=['GET', 'POST'])
@@ -90,19 +108,23 @@ def text_mail():
         mails_list = []
         file = request.form['upload-file']
         print("FIle",file)
-        with open(file) as f:
-            csvfile = csv.reader(f)
-            print(csvfile)
-            for row in csvfile:
-                mails_list.append(row[0])
-        for i in mails_list:
+        #with open(file) as f:
+         #   csvfile = csv.reader(f)
+          #  print(csvfile)
+           # for row in csvfile:
+            #    mails_list.append(row[0])
+        df = pd.read_csv(file)
+        count = 1
+        for i in df.mails:
             fromaddr = request.form.get('sender')
             toaddr = i
+            print(count," : ",i)
             msg = MIMEMultipart() 
             msg['From'] = fromaddr
             msg['To'] = toaddr
             msg['Subject'] = request.form.get('subject')
             body = request.form.get('mailbody')
+            #print(body)
             msg.attach(MIMEText(body, 'plain'))
             s = smtplib.SMTP('smtp.gmail.com', 587)
             s.starttls()
@@ -111,9 +133,10 @@ def text_mail():
             text = msg.as_string()
             s.sendmail(fromaddr, toaddr, text)
             s.quit()
+            count += 1
         confirm = 'Mail Has been sent'
     
-        return render_template("index.html",confirm = confirm)
+        return render_template("home.html",confirm = confirm)
 
 # Mail with name and certificate
 # Need to upload text file and .csv file
@@ -126,7 +149,9 @@ def mail_with_names():
         df = pd.read_csv(file)
         #print(df)
         dic = dict(zip(df.mails,df.names))
+        count= 1
         for i in dic:
+            print(count," : ",i)
             fromaddr = request.form.get('sender')
             toaddr = i
             msg = MIMEMultipart() 
@@ -157,12 +182,15 @@ def mail_with_names():
             text = msg.as_string()
             s.sendmail(fromaddr, toaddr, text)
             s.quit()
+            count += 1
         confirm = 'Mail Has been sent'
     
-        return render_template("index.html",confirm = confirm)
+        return render_template("home.html",confirm = confirm)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+# Participating Certificate for the Workshop With TechnologyForAll
     
     
    
